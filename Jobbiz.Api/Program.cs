2f7db060ecef23ac1a893acd0e04ae670c5ee658
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-18 14:26:02 -0300

    feat: register DbContext in the builder's service

--- Jobbiz.Api/Program.cs
+++ Jobbiz.Api/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Jobbiz.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Adds the db context so the app can access it
+builder.Services.AddDbContext<JobbizDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
