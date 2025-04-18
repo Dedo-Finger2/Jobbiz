741b3f8a5e3a6b38648e58d6a030485dd40b88aa
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-18 14:24:08 -0300

    feat: adds DbContext

---
+++ Jobbiz.Api/JobbizDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Jobbiz.Api
+{
+    public class JobbizDbContext : DbContext
+    {
+        public JobbizDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        // Setting up tables. Each one is always a dbSet
+        // Here we got the Vacancies table
+        public DbSet<VacancyEntity> Vacancies { get; set; }
+    }
+}
