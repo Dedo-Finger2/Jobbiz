b781d716ff048a87677120a1e656da05c2823089
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-18 15:02:41 -0300

    feat: adds OpenAPI + Scalar integration + GET vacancies

--- Jobbiz.Api/Program.cs
+++ Jobbiz.Api/Program.cs
@@ -1,13 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using Jobbiz.Api;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Adds the db context so the app can access it
 builder.Services.AddDbContext<JobbizDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+var v1 = app.MapGroup("/api/v1");
+
+v1.MapGet("/vacancies", async (JobbizDbContext db) =>
+{
+    try
+    {
+        return Results.Ok(await db.Vacancies.ToListAsync());
+    } catch (Exception e)
+    {
+        Console.WriteLine(e);
+
+        return Results.InternalServerError("try again later");
+    }
+});
 
 app.Run();
